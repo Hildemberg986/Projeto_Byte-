412952b8e7d934861f28f7b9f94b9339e8f0c650
Hildemberg Eling <hildembergeling@gmail.com>
2023-04-22 18:08:17 -0300

    import controller and router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import controller from './controller'
+import Router from '@/router'
 
-createApp(App).mount('#app')
+createApp(App).use(controller).use(Router).mount('#app')
